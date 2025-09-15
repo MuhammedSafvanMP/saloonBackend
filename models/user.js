467c20a52c40d3e7f239890b2b2a8c0d64ce3f7c
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2025-09-15 09:03:45 +0530

    feat: user updated

--- models/user.js
+++ models/user.js
@@ -1,34 +1,38 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     image: {
-        type: String, 
+      type: String,
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phone: {
-         type: String,
-         unique: true 
+      type: String,
+      unique: true,
     },
     role: {
-        enum: ["user", "admin", "owner"]
+      type: String,
+      enum: ["user", "admin", "owner"],
+      default: "user",
     },
     subscription: {
-        type: Boolean,
-        default: true
-    }
-
-}, { timestamps: true });
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
